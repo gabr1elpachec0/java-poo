1ccdf05e5473ccab99296ba6250652b1aba251c7
gabr1elpachec0 <gabrielgpacheco12@gmail.com>
2024-03-12 13:19:29 -0300

    first commit

---
+++ src/tipos_primitivos/Exercicio1.java
@@ -0,0 +1,15 @@
+package tipos_primitivos;
+
+public class Exercicio1 {
+    public static void main (String[] args) {
+        int jan = 15000;
+        int fev = 23000;
+        int mar = 17000;
+
+        int media = (jan + fev + mar) / 3;
+        int despesa_total = jan + fev + mar;
+
+        System.out.println("Despesa total = " + despesa_total);
+        System.out.println("Media mensal = " + media);
+    }
+}
---
+++ src/tipos_primitivos/ExercicioX.java
@@ -0,0 +1,51 @@
+package tipos_primitivos;
+
+public class ExercicioX {
+    public static void main(String[] args) {
+        /*
+        for (int i = 150; i <= 300; i += 1) {
+            System.out.println(i);
+        }
+        */
+
+        /*
+        int soma = 0;
+        for (int i = 1; i <= 1000; i += 1) {
+            soma += i;
+            System.out.println(soma);
+        }
+        */
+
+        /*
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+        */
+
+        /*
+        long fatorial = 1;
+        for (int i = 1; i <= 40; i += 1) {
+            fatorial *= i;
+            System.out.println(fatorial);
+        }
+        */
+
+        // Fibonacci
+        /*
+        int num1 = 1, num2 = 0;
+
+        System.out.println(num2);
+        System.out.println(num1);
+
+        for (int i = 0; i <= 100; i +=1) {
+            num1 = num1 + num2;
+            num2 = num1 - num2; // 0, 1, 1, 2, 3
+            System.out.println(num1);
+        }
+        */
+
+    }
+
+}
